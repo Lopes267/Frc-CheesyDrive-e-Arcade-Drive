89e0e1bd0f5298552529a5a55798e14f287bb9e1
Lopes <Enzopaula05@gmail.com>
2025-08-29 22:51:58 -0300

    Todos os arquivos

---
+++ drivetrain_cheesy_Drive.py
@@ -0,0 +1,28 @@
+import wpilib
+import wpilib.drive
+from rev import CANSparkMax, CANSparkMaxLowLevel
+
+class DriveTrain_cheesy_Drive():
+    def __init__(self):
+        self.left_front_motor = CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushed)
+        self.left_back_motor = CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushed)
+        self.right_front_motor = CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushed)
+        self.right_back_motor = CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushed)
+
+        # Climber motors
+        self.climber_right_motor = CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushed)
+        self.climber_left_motor = CANSparkMax(6, CANSparkMaxLowLevel.MotorType.kBrushed)
+        self.climber_right_motor.setInverted(True)
+        self.climber_motors = wpilib.MotorControllerGroup(self.climber_right_motor, self.climber_left_motor)
+
+        # Drive motors
+        self.left_motors = wpilib.MotorControllerGroup(self.left_front_motor, self.left_back_motor)
+        self.right_motors = wpilib.MotorControllerGroup(self.right_front_motor, self.right_back_motor)
+
+        self.right_motors.setInverted(True)
+        self.drivetrain = wpilib.drive.DifferentialDrive(self.left_motors, self.right_motors)
+
+    def cheesyDrive(self, speed: float, rotation: float, quickTurn: bool):
+        self.drivetrain.curvatureDrive(speed, rotation, quickTurn)
+            
+
