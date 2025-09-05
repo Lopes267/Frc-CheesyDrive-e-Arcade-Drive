2bcd458f60fad380c8d73ba9d547b6974dbd5ad1
Lopes <Enzopaula05@gmail.com>
2025-09-05 08:44:03 -0300

    atualizado

--- climber_motors.py
+++ climber_motors.py
@@ -5,8 +5,10 @@ import wpilib.drive
 
 class Climber():
     def __init__(self):
-         self.climber_right_motor = rev.sparkMax(5, rev.SparkMaxLowLevel.MotorType.kBrushed)
+         self.climber_right_motor = rev.SparkMax(5, rev.SparkMaxLowLevel.MotorType.kBrushed)
          self.climber_left_motor = rev.SparkMax(6, rev.SparkMaxLowLevel.MotorType.kBrushed)
          self.climber_right_motor.setInverted(True)
          self.climber_motors = wpilib.MotorControllerGroup(self.climber_right_motor, self.climber_left_motor)
+    def climber_motor(self,speed):
+         self.climber_motors.set(speed)
     
