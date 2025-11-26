46421ed10cc2258e2b7083eb3dbd20a0972492d0
Lopes <Enzopaula05@gmail.com>
2025-11-26 08:14:23 -0300

    V.3

--- drivetrain.py
+++ drivetrain.py
@@ -4,15 +4,12 @@ import rev
 
 class DriveTrain():
     def __init__(self):
-        self.left_front_motor = rev.SparkMax(1, rev.SparkMaxLowLevel.MotorType.kBrushed)
-        self.left_back_motor = rev.SparkMax(2, rev.SparkMaxLowLevel.MotorType.kBrushed)
-        self.right_front_motor = rev.SparkMax(3, rev.SparkMaxLowLevel.MotorType.kBrushed)
-        self.right_back_motor = rev.SparkMax(4, rev.SparkMaxLowLevel.MotorType.kBrushed)
+        self.left_front_motor = rev.SparkMax(1, rev.SparkLowLevel.MotorType.kBrushed)
+        self.left_back_motor = rev.SparkMax(2, rev.SparkLowLevel.MotorType.kBrushed)
+        self.right_front_motor = rev.SparkMax(3, rev.SparkLowLevel.MotorType.kBrushed)
+        self.right_back_motor = rev.SparkMax(4, rev.SparkLowLevel.MotorType.kBrushed)
 
-    
         
-
-        #Drive Motors
         self.left_motors = wpilib.MotorControllerGroup(self.left_front_motor, self.left_back_motor)
         self.right_motors = wpilib.MotorControllerGroup(self.right_front_motor, self.right_back_motor)
         self.drivetrain = wpilib.drive.DifferentialDrive(self.left_motors, self.right_motors)
--- robot.py
+++ robot.py
@@ -1,8 +1,11 @@
+import commands2
 import wpilib
 import wpilib.drive
 import rev
 from drivetrain import DriveTrain
 from climber_motors import Climber
+from wpilib import SlewRateLimiter
+
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
@@ -10,21 +13,31 @@ class Robot(wpilib.TimedRobot):
         self.joystick = wpilib.Joystick(0)
         self.timer = wpilib.Timer()
         self.climber = Climber()
+        self.slew_limiter = SlewRateLimiter(2) 
+        
 
     def teleopInit(self):
         pass
 
     def teleopPeriodic(self):
-        speed = -self.joystick.getRawAxis(1)
+        
+        raw_speed = -self.joystick.getRawAxis(1)
         rotation = self.joystick.getRawAxis(2)
+        current = self.slew_limiter.lastValue 
+        if raw_speed > current:             #calcula para que se tenha uma aceleracao suave
+            speed = self.slew_limiter.calculate(raw_speed) 
+        else:                               #Freada Rapida
+            speed = raw_speed
+            self.slew_limiter.reset(raw_speed)
         self.drive.arcadeDrive(speed, rotation)
+        
 
         if self.joystick.getRawButton(1):
-            self.drive.climber_motors.set(0.5)
+            self.climber.climber_motor(0.5)
         elif self.joystick.getRawButton(2):
-            self.drive.climber_motors.set(-0.5)
+            self.climber.climber_motor(-0.5)
         else: 
-            self.drive.climber_motors.set(0)
+            self.climber.climber_motor(0)
 
     def autonomousInit(self):
         self.timer.reset()
@@ -34,12 +47,12 @@ class Robot(wpilib.TimedRobot):
         if self.timer.get() <= 7.5:
             self.drive.arcadeDrive (0.5, 0)
         elif 7.6 <= self.timer.get() <= 9.0:
-            self.drive.climber_motors.set(0.5)
+            self.climber.climber_motor(0.5)
         elif 9.0 < self.timer.get() <= self.autonomous_duration:
             self.drive.arcadeDrive(-0.5, 0)
         else:
             self.drive.arcadeDrive(0, 0)
-            self.drive.climber_motors.set(0)
+            self.climber.climber_motor(0)
     
 
 
